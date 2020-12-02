1ccad42f0acc8cac1cfe8e6dd13e31f2a8323052
Brady Douthit <brady.douthit99@gmail.com>
2020-12-02 17:00:51 -0500

    add directory where images are stored

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const axios = require('axios');
 const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
 
 //use bodyParser
 app.use(bodyParser.json());
@@ -10,7 +10,32 @@ app.use(bodyParser.json());
 // listen for requests
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a POST route
+// create a route to screenshot the requested URL and send it back to the front end
 app.post('/scrape', (req, res) => {
-  console.log(req.body)
+  let url = req.body.url;
+  console.log(`${__dirname}/client/src/img/${url}`);
+  (async () => {
+    try {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+      });
+
+      const page = await browser.newPage();
+      await page.goto(`http://${url}`);
+
+      const image = await page.screenshot(
+        { 
+          fullPage: true ,
+          path: `${__dirname}/client/src/img/${url}.png`
+        });
+
+      await browser.close();
+
+      res.set('Content-Type', 'image/png');
+      res.send(image);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  })();
 });
